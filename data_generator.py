f14fcc1f7a099576203377c5f4e0e76a5df2a08a
Twenzel <tomwenzel@hotmail.de>
2022-07-28 12:52:43 +0200

    added recognition for empty fields to data set generator

--- data_generator.py
+++ data_generator.py
@@ -2,9 +2,12 @@ import random
 
 import cv2
 import numpy as np
+from keras.models import load_model
 
-from main import get_corners, slice_image
+from main import get_corners, slice_image, predict_chesspieces
 from utility_functions.image_downloader import download_image
+
+model = load_model('models/model_5000_blue_red.h5')
 
 if __name__ == '__main__':
     image = download_image()
@@ -13,11 +16,11 @@ if __name__ == '__main__':
     flattened_list = [element for sublist in sliced_board for element in sublist]
     x = np.array([np.array(cv2.resize(image['image'], dsize=(224, 224), interpolation=cv2.INTER_AREA)) for image in
                   flattened_list])
+    prediction_list = predict_chesspieces(model=model, field=sliced_board, reshape=(8, 8))
+    flattened_list_predictions = [element for sublist in prediction_list for element in sublist]
 
     counter = 0
     for i in x:
-        counter += 1
-        if counter < 17 or counter > 48:
+        if flattened_list_predictions[counter] != 0:
             cv2.imwrite(f'images/{counter}_{random.randint(0, 1000)}.png', i)
-
-
+        counter += 1
